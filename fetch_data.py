6543c0db819fe74257f44edfac010ed03f5b8aec
Gustavo Trigos <gustavoa.trigos@Guss-MacBook-Pro.local>
2020-03-16 08:00:52 -0700

    gets timeseries data into csv format directly from JHU's COVID-19 github repository.

---
+++ fetch_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def fetch_data():
+
+	df_confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+	df_death = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
+	df_recovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
+
+	return df_confirmed, df_death, df_recovered
+
+# Storing data into variables
+df_confirmed, df_death, df_recovered = fetch_data()
